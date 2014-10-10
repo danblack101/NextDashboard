9946d87e254197d84721dc5cf02989685ead1688
Dan Black <dblack@boardbooks.com>
2014-10-10 16:39:53 +1300

    ui conditional formatting

--- UI.Host/Program.cs
+++ UI.Host/Program.cs
@@ -8,14 +8,14 @@ namespace UI.Host
     internal class Program
     {
         private static readonly ILogger _logger = new LoggerAdapter(LogManager.GetCurrentClassLogger());
-        static readonly IConfigurationManager _configurationManager = new ConfigurationManagerWrapper();
+        static readonly IConfigurationManager ConfigurationManager = new ConfigurationManagerWrapper();
         private static void Main(string[] args)
         {
-            var baseAddress = _configurationManager.GetSetting("OwinEndPoint");
+            var baseAddress = ConfigurationManager.GetSetting("OwinEndPoint");
 
             using (WebApp.Start<Startup>(baseAddress))
             {
-                _logger.Info("Dashboard WebServer running. Press any key to exit");
+                _logger.Info("Dashboard WebServer running. Press <enter> key to exit");
                 Console.ReadLine();
             }
         }
